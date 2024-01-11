c729557413d0aa4681d9aec86efd11ce9038af82
Wiraa <ur.wiraananda@gmail.com>
2024-01-12 01:43:52 +0800

    sampai di post

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,80 +1,139 @@
 "use client";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { usePasien } from "@/hooks/pasien/useFetchPasien";
+import { axiosInstance } from "@/lib/axios";
 import {
-  Container,
-  Heading,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-  Spinner,
   FormControl,
   FormLabel,
   Input,
   VStack,
   Button,
-  useToast,
+  Select,
 } from "@chakra-ui/react";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <AddPasien />
-      </div>
-    </main>
-  );
-}
+import { useFormik } from "formik";
+import { useState, useEffect } from "react";
 
 function AddPasien() {
+  const [submitter, setSubmitter] = useState(false);
   const { pasien } = usePasien();
+
   const renderData = () => {
     return pasien?.map((item) => (
       <tr key={item.noPasien}>
         <td>{item.noPasien}</td>
         <td>{item.nama}</td>
-        <td>{item.keluhan}</td>
         <td>{item.gender}</td>
         <td>{item.alamat}</td>
+        <td>{item.keluhan}</td>
         <td>{item.kunjungan}</td>
       </tr>
     ));
   };
 
+  const formik = useFormik({
+    initialValues: {
+      nama: "",
+      gender: "",
+      alamat: "",
+      keluhan: "",
+    },
+    onSubmit: async () => {
+      const { nama, gender, alamat, keluhan } = formik.values;
+      setSubmitter(true);
+
+      try {
+        await axiosInstance.post("/pasien", {
+          nama,
+          gender,
+          alamat,
+          keluhan,
+        });
+        formik.resetForm();
+        window.location.reload();
+      } catch (error) {
+        console.error("Submit Gagal: ", error);
+      } finally {
+        setSubmitter(false);
+      }
+    },
+  });
+
+  useEffect(() => {
+    console.log("Submit Berhasil");
+  }, [submitter]);
+
+  const handleFormValue = (event) => {
+    const { name, value } = event.target;
+
+    formik.setFieldValue(name, value);
+  };
+
+  const option = ["P", "L"];
+
   return (
     <div className="w-full mx-[5rem] block">
-      <h1 className="font-bold text-[1rem] mb-[.5rem]">DATA PASIEN</h1>
-      <table className="w-full rounded-lg text-center mb-[1rem] py-[.5rem]">
+      <h1 className="font-bold text-[1.5rem] mb-[.5rem] text-center">
+        DATA PASIEN
+      </h1>
+      <table className="w-full rounded-lg text-center mb-[2rem] py-[.5rem]">
         <thead>
           <tr>
             <th>ID</th>
             <th>Nama</th>
-            <th>Keluhan</th>
             <th>Gender</th>
             <th>Alamat</th>
+            <th>Keluhan</th>
             <th>Kunjungan</th>
           </tr>
         </thead>
         <tbody>{renderData()}</tbody>
-        <form action="">
-          <VStack>
-            <FormControl>
-              <FormLabel></FormLabel>
-              <Input />
-            </FormControl>
-          </VStack>
-          <VStack>
-            <FormControl>
-              <FormLabel></FormLabel>
-              <Input />
-            </FormControl>
-          </VStack>
-        </form>
       </table>
+      <h1 className="font-bold text-[1.5rem] mb-[.5rem] text-center">
+        SUBMIT DATA PASIEN
+      </h1>
+      <form onSubmit={formik.handleSubmit}>
+        <VStack spacing={3}>
+          <FormControl>
+            <FormLabel>Nama</FormLabel>
+            <Input onChange={handleFormValue} name="nama" />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Gender</FormLabel>
+            <Select
+              name="gender"
+              value={formik.values.gender}
+              onChange={formik.handleChange}
+            >
+              {option.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Alamat</FormLabel>
+            <Input onChange={handleFormValue} name="alamat" />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Keluhan</FormLabel>
+            <Input onChange={handleFormValue} name="keluhan" />
+          </FormControl>
+          <Button type="submit">Submit</Button>
+        </VStack>
+      </form>
     </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-center font-bold text-[2rem] mb-[3rem]">
+          MONITORING DATA PASIEN RUMAH SAKIT WIRAA CORPORATION
+        </h1>
+        <AddPasien />
+      </div>
+    </main>
   );
 }
